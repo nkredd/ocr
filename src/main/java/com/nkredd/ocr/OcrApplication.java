99bf35d3e45301a0dfbbf612a9fe7efbfa45cb0f
nkredd <nel2703@gmail.com>
2018-11-23 14:17:52 -0300

    Criando operacao de instanciacao

--- src/main/java/com/nkredd/ocr/OcrApplication.java
+++ src/main/java/com/nkredd/ocr/OcrApplication.java
@@ -1,12 +1,30 @@
 package com.nkredd.ocr;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.nkredd.ocr.domain.Campus;
+import com.nkredd.ocr.repositories.CampusRepository;
+
 @SpringBootApplication
-public class OcrApplication {
+public class OcrApplication implements CommandLineRunner{
+	@Autowired
+	private CampusRepository campusRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OcrApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Campus campus1 = new Campus(null, "Euclides da Cunha");
+		Campus campus2 = new Campus(null, "Feira de Santana");
+		
+		campusRepository.saveAll(Arrays.asList(campus1, campus2));
+		
 	}
 }
