8796067a9bd74baa887ed91178c97b58cdb9a68c
nkredd <nel2703@gmail.com>
2018-11-23 11:38:29 -0300

    Testando primeira classe de dominio

--- src/main/java/com/nkredd/ocr/resources/CampusResource.java
+++ src/main/java/com/nkredd/ocr/resources/CampusResource.java
@@ -1,15 +1,26 @@
 package com.nkredd.ocr.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nkredd.ocr.domain.Campus;
 
 @RestController
 @RequestMapping(value="/campus")
 public class CampusResource {
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Campus> listar() {
+		Campus campus1 = new Campus(1, "Euclides da Cunha");
+		Campus campus2 = new Campus(2, "Feira de Santana");
+		
+		List<Campus> list = new ArrayList<Campus>();
+		list.add(campus1);
+		list.add(campus2);		
+		return list;
 	}
 
 }
